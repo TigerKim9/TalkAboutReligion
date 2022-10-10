746a4618916175286e447841500c2d7b9adbda04
TigerKim9 <kkm9499@gmail.com>
2022-10-10 23:48:47 +0900

    종교 평가 서비스 틀 제작

---
+++ src/main/java/com/religion/TalkAboutReligionApplication.java
@@ -0,0 +1,13 @@
+package com.religion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkAboutReligionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkAboutReligionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/religion/TalkAboutReligionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.religion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkAboutReligionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
