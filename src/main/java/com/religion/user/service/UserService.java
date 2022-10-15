75b1a0d01085043f76011dee8f731fdaafb20a06
TigerKim9 <kkm9499@gmail.com>
2022-10-15 23:43:45 +0900

    User service and Mapper

---
+++ src/main/java/com/religion/user/service/UserService.java
@@ -0,0 +1,17 @@
+package com.religion.user.service;
+
+import org.springframework.stereotype.Service;
+
+import com.religion.user.UserDTO;
+
+@Service
+public class UserService {
+
+	UserDTO user;
+	UserMapper umapper;
+	
+	public int registUser(UserDTO user) {
+		user
+	}
+	
+}
