dba8300f0e0cdf599fdd1413f15905791b857897
TigerKim9 <kkm9499@gmail.com>
2022-10-18 15:29:38 +0900

    파일 전체 변환

--- src/test/java/com/religion/TalkAboutReligionApplicationTests.java
+++ src/test/java/com/religion/TalkAboutReligionApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TalkAboutReligion1ApplicationTests {
+class TalkAboutReligionApplicationTests {
 
 	@Test
 	void contextLoads() {
